74073d4b31602875100c5b1cd2a6ee8671af2deb
Petteri Huuskonen <petzku.huuskonen@gmail.com>
2019-06-07 00:28:51 +0300

    Add a more readable format for verbose print

--- helpers.py
+++ helpers.py
@@ -13,3 +13,15 @@ async def latest_doodle_reactions(ctx, bot_user):
 async def get_starting_lineup(guild):
     all_members = guild.members
     return list(set(i.name for i in all_members if any(j.name.lower() in ["player", "trial"] for j in i.roles)))
+
+def verbose_doodle_output(names, days, ready):
+    WORKS = 'x'
+    NOTOK = '.'
+
+    namelen = max(len(n) for n in names)
+    header = ' ' * (namelen + 1) + ' '.join("{0:s}({1:d})".format(day, len(days[day])) for day in PLAIN_DAYS)
+    lines = ("{name:{width}s} ".format(name=name, width=namelen)
+                + ' '.join("  {}  ".format(WORKS if name in days[day] else NOTOK) for day in PLAIN_DAYS)
+                for name in names)
+
+    return "```\n{header}\n{status}\n```".format(header=header, status='\n'.join(lines))
